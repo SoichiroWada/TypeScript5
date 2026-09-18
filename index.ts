0fc3d1366b670be4c4b01cd4d439ea5f24836433
Soichiro Wada <soichirowada@ipmulticast.net>
2026-09-19 03:24:39 +0900

    typescript variables

--- index.ts
+++ index.ts
@@ -2,8 +2,13 @@
 // const age = 30
 
 let age: number = 44
+let firstName: string = "Mario"
+let isFictional: boolean
 
 age = 55;
+
+firstName = 'Luigi'
+isFictional = true
 
 console.log(age)
 // console.log(firstName, age)

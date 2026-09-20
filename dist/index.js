057c64604b197d7a9bde588ae3445bba3cc29f7c
Soichiro Wada <soichirowada@ipmulticast.net>
2026-09-21 01:46:29 +0900

    Add type aliases, interfaces, and functions for color and user management

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,92 @@
 "use strict";
+//--------------
+// type aliases
+//--------------
+Object.defineProperty(exports, "__esModule", { value: true });
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return [r, g, b];
+}
+const colorOne = getRandomColor();
+const colorTwo = getRandomColor();
+console.log(colorOne, colorTwo);
+const userOne = { name: 'mario', score: 75 };
+function formatUser(user) {
+    console.log(`${user.name} has a score of ${user.score}.`);
+}
+formatUser(userOne);
+formatUser({ name: 'yoshi', score: 100 });
+const authorOne = { name: 'mario', avatar: '/img/mario.png' };
+const authorTwo = { name: 'Taro', avatar: '/img/doki.png' };
+const time = new Date().toLocaleString();
+const time2 = new Date().toLocaleDateString();
+const time3 = new Date().toLocaleTimeString();
+console.log(time);
+console.log(time2);
+console.log(time3);
+const newPost = {
+    title: 'my first post',
+    body: 'something interesting',
+    tags: ['gaming', 'tech'],
+    created_at: new Date(),
+    author: authorOne,
+};
+const secondPost = {
+    title: 'my second post',
+    body: 'something getting worse',
+    tags: ['gaming', 'tech'],
+    created_at: new Date(),
+    author: authorTwo,
+};
+//----------------------------
+// as function argument types
+//----------------------------
+function createPost(post) {
+    console.log(`created post ${post.title} by ${post.author.name}`);
+}
+// createPost({ title: 'a new post title' })
+createPost(newPost);
+createPost(secondPost);
+//-------------
+// with arrays
+//-------------
+let posts = [];
+// posts.push({ title: 'some title' })
+posts.push(newPost);
+console.log(posts);
+posts.push(secondPost);
+console.log(posts);
+//-----------------------
+// return type inference
+//-----------------------
+function formatGreeting(name, greeting) {
+    return `${greeting}, ${name}`;
+}
+console.log(formatGreeting("Bundoka", "Aloha!"));
+// we get inference on return types, but not on argument types
+// type inference on return values does not enforce a return type
+//-----------
+// functions
+//-----------
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+const subtractTwoNumbers = (a, b) => {
+    return a - b;
+};
+// addTwoNumbers('2', 5)
+addTwoNumbers(3, 9);
+subtractTwoNumbers(10, 7);
+function addAllNumbers(items) {
+    const total = items.reduce((a, c) => a + c, 0);
+    console.log("total:", total);
+}
+addAllNumbers([5, 7, 9, 11, 3, 2, 1]);
 //------------------------
 // basic type annotations
 //------------------------
-Object.defineProperty(exports, "__esModule", { value: true });
 let ageOne = 30;
 let firstName = 'Mario';
 let isFictional;
@@ -87,4 +171,3 @@ let person = {
 person.name = 'bowser';
 const score = person.score; // infers number type
 //# sourceMappingURL=index.js.map
-console.log(score)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,143 @@
+//--------------
+// type aliases
+//--------------
+
+// example 1 - tuple
+
+type Rgb = [number, number, number]
+
+function getRandomColor(): Rgb {
+	const r = Math.floor(Math.random() * 255)
+	const g = Math.floor(Math.random() * 255)
+	const b = Math.floor(Math.random() * 255)
+
+	return [r, g, b]
+}
+
+const colorOne = getRandomColor()
+const colorTwo = getRandomColor()
+console.log(colorOne, colorTwo)
+
+// example 2 - object literal
+
+type User = {
+	name: string
+	score: number
+}
+
+const userOne: User = { name: 'mario', score: 75 }
+
+function formatUser(user: User): void {
+	console.log(`${user.name} has a score of ${user.score}.`)
+}
+
+formatUser(userOne)
+formatUser({ name: 'yoshi', score: 100 })
+
+//------------
+// interfaces
+//------------
+
+interface Author {
+	name: string
+	avatar: string
+}
+
+const authorOne: Author = { name: 'mario', avatar: '/img/mario.png' }
+const authorTwo: Author = { name:'Taro', avatar: '/img/doki.png'}
+
+const time = new Date().toLocaleString()
+const time2 = new Date().toLocaleDateString()
+const time3 = new Date().toLocaleTimeString()
+console.log(time)
+console.log(time2)
+console.log(time3)
+
+interface Post {
+	title: string
+	body: string
+	tags: string[]
+	created_at: Date
+	author: Author
+}
+
+const newPost:Post = {
+	title: 'my first post',
+	body: 'something interesting',
+	tags: ['gaming', 'tech'],
+	created_at: new Date(),
+	author: authorOne,
+}
+
+const secondPost = {
+	title: 'my second post',
+	body: 'something getting worse',
+	tags: ['gaming', 'tech'],
+	created_at: new Date(),
+	author: authorTwo,
+}
+
+//----------------------------
+// as function argument types
+//----------------------------
+
+function createPost(post: Post): void {
+	console.log(`created post ${post.title} by ${post.author.name}`)
+}
+
+// createPost({ title: 'a new post title' })
+createPost(newPost)
+createPost(secondPost)
+
+//-------------
+// with arrays
+//-------------
+
+let posts: Post[] = []
+
+// posts.push({ title: 'some title' })
+posts.push(newPost)
+console.log(posts)
+posts.push(secondPost)
+console.log(posts)
+
+//-----------------------
+// return type inference
+//-----------------------
+
+function formatGreeting(name: string, greeting: string) {
+	return `${greeting}, ${name}`
+}
+
+console.log(formatGreeting("Bundoka", "Aloha!"))
+
+// we get inference on return types, but not on argument types
+// type inference on return values does not enforce a return type
+
+
+//-----------
+// functions
+//-----------
+
+function addTwoNumbers(a: number, b: number): number {
+	return a + b
+}
+
+const subtractTwoNumbers = (a: number, b: number): number => {
+	return a - b
+}
+
+// addTwoNumbers('2', 5)
+addTwoNumbers(3, 9)
+subtractTwoNumbers(10, 7)
+
+function addAllNumbers(items: number[]): void {
+	const total = items.reduce((a, c) => a + c, 0)
+    console.log("total:",total);
+}
+
+addAllNumbers([5, 7, 9, 11, 3, 2, 1])
+
 //------------------------
 // basic type annotations
 //------------------------

d0f05e51e1d4cee9b786a3ec2bdc470c2f3374a1
Soichiro Wada <soichirowada@ipmulticast.net>
2026-09-22 18:04:20 +0900

    feat: extend interfaces and type aliases for user and bill management

---
+++ dist/index3.js
@@ -0,0 +1,24 @@
+"use strict";
+//------------------------
+// extending type aliases
+//------------------------
+Object.defineProperty(exports, "__esModule", { value: true });
+const personOne = {
+    id: 1,
+    firstName: 'mario',
+};
+const personTwo = {
+    id: '2',
+    firstName: 'yoshi',
+    email: 'yoshi@netninja.dev',
+};
+const personThree = {
+    email: 'peach@netninja.dev',
+};
+function printUser(user) {
+    console.log(user.id, user.email, user.firstName);
+}
+// printUser(personOne) --> not allowed
+printUser(personTwo);
+// printUser(personThree) --> not allowed
+//# sourceMappingURL=index3.js.map

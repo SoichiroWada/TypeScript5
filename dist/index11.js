e3947d0617d414316451a01acfff53d1d70bd643
Soichiro Wada <soichirowada@ipmulticast.net>
2026-09-25 00:39:27 +0900

    feat: add test.csv and enhance CSVWriter with logging and updated payment data

--- dist/index11.js
+++ dist/index11.js
@@ -8,20 +8,29 @@ class CSVWriter {
     constructor(columns) {
         this.columns = columns;
         this.csv = this.columns.join(',') + '\n';
+        console.log("this.columns:", this.columns);
+        console.log("this.csv:", this.csv);
     }
     csv;
     addRows(values) {
+        console.log("values:", values);
         let rows = values.map((v) => this.formatRow(v));
+        console.log("rows:", rows);
         this.csv += rows.join('\n');
-        console.log(this.csv);
+        console.log("this.csv in addRows:", this.csv);
     }
-    formatRow(p) {
-        return this.columns.map((col) => p[col]).join(',');
+    formatRow(object) {
+        const text = this.columns.map((col) => object[col]).join(',');
+        console.log("text:", text);
+        return text;
+        // return this.columns.map((col) => object[col]).join(',')
     }
 }
 const writer = new CSVWriter(['id', 'amount', 'to', 'notes']);
+console.log("writer:", writer);
 writer.addRows([
-    { id: 1, amount: 50, to: 'yoshi', notes: 'for design work' },
-    { id: 2, amount: 50, to: 'yoshi', notes: 'for design work' },
+    { id: 1, amount: 11, to: 'yoshi', notes: 'for design work' },
+    { id: 2, amount: 22, to: 'kintaro', notes: 'special minilary operation' },
+    { id: 3, amount: 35, to: 'bonbigaso', notes: 'repair my car' },
 ]);
 //# sourceMappingURL=index11.js.map
--- src/index11.ts
+++ src/index11.ts
@@ -3,37 +3,43 @@
 //--------------------
 
 interface Payment {
-	id: number
-	amount: number
-	to: string
-	notes: string
+    id: number
+    amount: number
+    to: string
+    notes: string
 }
 
 type PaymentColumns = ('id' | 'amount' | 'to' | 'notes')[]
 
 class CSVWriter {
-	constructor(private columns: PaymentColumns) {
-		this.csv = this.columns.join(',') + '\n'
-	}
+    constructor(private columns: PaymentColumns) {
+        this.csv = this.columns.join(',') + '\n'
+        console.log("this.columns:", this.columns)
+        console.log("this.csv:", this.csv)
+    }
 
-	private csv: string
+    private csv: string
 
-	addRows(values: Payment[]): void {
-		let rows = values.map((v) => this.formatRow(v))
+    addRows(values: Payment[]): void {
+        console.log("values:", values)
+        let rows = values.map((v) => this.formatRow(v))
+        console.log("rows:", rows)
+        this.csv += rows.join('\n')
+        console.log("this.csv in addRows:", this.csv)
+    }
 
-		this.csv += rows.join('\n')
-
-		console.log(this.csv)
-	}
-
-	private formatRow(p: Payment): string {
-		return this.columns.map((col) => p[col]).join(',')
-	}
+    private formatRow(object: Payment): string {
+        const text = this.columns.map((col) => object[col]).join(',')
+        console.log("text:",text)
+        return text
+        // return this.columns.map((col) => object[col]).join(',')
+    }
 }
 
 const writer = new CSVWriter(['id', 'amount', 'to', 'notes'])
-
+console.log("writer:",writer)
 writer.addRows([
-	{ id: 1, amount: 50, to: 'yoshi', notes: 'for design work' },
-	{ id: 2, amount: 50, to: 'yoshi', notes: 'for design work' },
+    { id: 1, amount: 11, to: 'yoshi', notes: 'for design work' },
+    { id: 2, amount: 22, to: 'kintaro', notes: 'special minilary operation' },
+    { id: 3, amount: 35, to: 'bonbigaso', notes: 'repair my car' },
 ])
